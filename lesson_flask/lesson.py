a57f7a6c60799c0fec2dd8231d14f7b13ff949e8
Feam <maryking.textile@gmail.com>
2022-04-25 11:36:00 +0300

    ready

--- lesson_flask/lesson.py
+++ lesson_flask/lesson.py
@@ -1,27 +1,18 @@
 from flask import Flask, render_template
+from db_utils import get_session
+from variables import DEBUG, PORT
+from model import People
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
 
-@app.route("/avr_data")
-def get_avr_data():
-    with open("hw.csv", "r") as f:
-        a = f.readlines()[1:][:-1]
-        lst = []
-        ht = lst.append(sum([float(i.split(",")[1]) for i in a])
-                        * 2.54 / len(a))
-        wt = lst.append(sum([float(i.split(",")[2]) for i in a])
-                        * 0.4535 / len(a))
-        return render_template("std/get_add.html", stds=lst)
+@app.route("/list_name")
+def get_list_name():
+    s = get_session()
+    uniq_names = set([x.FirstName for x in s.query(People).all()])
 
-@app.route("/requirements")
-def get_requirements():
-    with open("requirements.txt","r", encoding="utf-16") as file:
-        my_txt = file.readlines()
-        return render_template("std/requirements.html", docs=my_txt)
+    return render_template('books/list.html', uniq_names=uniq_names)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG, port=PORT)
